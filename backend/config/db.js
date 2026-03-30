1ceb62f76451fc3f80dfd3ffeea10a865732c71d
Sriram <sriramsriram123654@gmail.com>
2026-03-30 19:38:22 +0530

    soul need to  update the db.js to connect db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      "mongodb://sriram:sriram@ac-6mjfmjg-shard-00-00.kxrrbkr.mongodb.net:27017,ac-6mjfmjg-shard-00-01.kxrrbkr.mongodb.net:27017,ac-6mjfmjg-shard-00-02.kxrrbkr.mongodb.net:27017/?ssl=true&replicaSet=atlas-pnxn7z-shard-0&authSource=admin&appName=smartinfra"
+      "mongodb://sriram:<password>@ac-6mjfmjg-shard-00-00.kxrrbkr.mongodb.net:27017,ac-6mjfmjg-shard-00-01.kxrrbkr.mongodb.net:27017,ac-6mjfmjg-shard-00-02.kxrrbkr.mongodb.net:27017/?ssl=true&replicaSet=atlas-pnxn7z-shard-0&authSource=admin&appName=smartinfra"
     );
 
     console.log("MongoDB Atlas Connected 🚀");
